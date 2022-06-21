5458230a70c25ffaba82a1e64705737fc1ad9056
alonspinner <alonspinner@gmail.com>
2022-06-21 16:10:08 +0300

    progress?

--- tests/08_hermiteCubicSplines.py
+++ tests/08_hermiteCubicSplines.py
@@ -1,30 +1,44 @@
-import sympy
-import numpy as np
-import matplotlib.pyplot as plt
 from symforce import sympy as sm
+from symforce import geo
 
-s = sympy.Symbol('s')
-p0 = 0 #starting point is [0,p0]
-m0 = 0
-p1 = 0 #ending point is [1,p1]
-m1 = -5 
+'''
+goal here is to create a semi factor showing what we want to do
 
+f(s,x1,x2,lm,r) -> residual
+s - running parameter
+xi - poses
+lm - landmark
+r - tunnel radius
+
+we want to build a hermite cubic spline from 2 poses to interpolate over the storage space.
+then we want to estimate the distance between the landmark and the spline, call it r_hat
+residual = r - rhat
+
+to construct the spline, we will use hermite cubic splines: https://en.wikipedia.org/wiki/Cubic_Hermite_spline
+they are basicaly the same as ferguson curves
+slopes will be the poses x directions.
+
+below we empoly a toy problem
+'''
+s = sm.Symbol("s")
+lm = geo.V3.symbolic("lm")
+x0 = geo.Pose3.symbolic("x0")
+x1 = geo.Pose3.symbolic("x1")
+r =sm.Symbol("r")
+
+#lets assume m derivatives were calculated for now...
+p0 = x0.t
+m0 = x0.R.to_rotation_matrix()[:,0]
+p1 = x1.t
+m1 = x1.R.to_rotation_matrix()[:,0]
+
+#lets create a curve with some running parameter u
 Lambda = (2*s**3-3*s**2+1) * p0 \
         + (s**3 - 2*s**2 + s) * m0 \
         + (-2*s**3 + 3*s**2) * p1 \
         + (s**3 - s**2) * m1
 
+rhat = (Lambda-lm).norm()
+res = rhat - r
 
-f = sympy.lambdify(s, Lambda)
-t = np.linspace(0,1,100)
-plt.plot(t,f(t))
-plt.show()
-
-# Lambda2D = sympy.Array([s,Lambda])
-# q = sympy.Array([sympy.Symbol(f"{v}") for v in ["q0", "q1"]])
-
-# #find sqrd norm
-# sqrdnorm = (q[0] - Lambda2D[0])** 2 + (q[1] - Lambda2D[1]) ** 2 #couldnt find vector operations.. whatever
-# sol = sympy.solve(sqrdnorm.diff(s),s)
-# sympy.exp(f"{sqrdnorm.diff(s)} == 0")
-
+print(res)
