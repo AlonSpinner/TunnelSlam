fb81ffedd4a694d8edf01bbe4525c65f20d9be38
Alon <alonspinner@gmail.com>
2022-11-09 15:30:52 +0200

    figured out the right twist?

--- tests/12_the_big_one.py
+++ tests/12_the_big_one.py
@@ -7,16 +7,27 @@ import matplotlib.pyplot as plt
 import tunnelslam.plotting as plotting
 
 TUNNEL_RADIUS = 1.0
-DT = 0.5 #constant time between keyframes
-T_final = 30.0
-twist = geo.V6([0.0,
-                0.0,
-                2 * np.pi/15, #full circle in 20 seconds
-                2.0,
-                0.0,
-                0.1]) #[omega, linear velocity]
+DT = 1.0 #constant time between keyframes
+T_final = 75.0
 
-p = geo.Pose3()
+R0 = geo.Rot3.from_yaw_pitch_roll(0,np.radians(10),0)
+t0 = geo.V3()
+p = geo.Pose3(R = R0, t = t0)
+
+w_world = geo.V3([0.0,0.0,2 * np.pi/15])
+w_robot = p.inverse() * w_world
+v_robot = geo.V3([-2.0,0.0,0.0])
+
+twist = geo.V6()
+twist[:3] = w_robot
+twist[3:] = v_robot
+
+# twist = geo.V6([0.05,
+#                 0.1,
+#                 2 * np.pi/15, #full circle in 15 seconds
+#                 2.0,
+#                 0.0,
+#                 0.0]) #[omega, linear velocity]
 
 #build tunnel and extract key poses - simulatation
 history = {"gt_p": [p],
@@ -24,9 +35,10 @@ history = {"gt_p": [p],
             "z" : [],
             "u" : []}
 time_from_last_keypose = 0.0
-for t in np.arange(0,T_final,DT/5):
+DT_TUNNELING = DT/3 
+for t in np.arange(0,T_final,DT_TUNNELING):
     #move robot
-    p = p * geo.Pose3.from_tangent(twist * DT)
+    p = p * geo.Pose3.from_tangent(twist * DT_TUNNELING)
 
     #perturb pose only on roll
     tangent_perturbation = np.zeros(6); 
@@ -37,18 +49,18 @@ for t in np.arange(0,T_final,DT/5):
     
     #store
     history["gt_l"].append(lm)
-    if time_from_last_keypose > DT:
+    if time_from_last_keypose >= DT:
         history["gt_p"].append(p)
         time_from_last_keypose = 0.0
     else:
-        time_from_last_keypose += DT
+        time_from_last_keypose += DT_TUNNELING
 
 #pass through tunnel - collect measurements
 
 #plot 
 fig = plt.figure()
 ax = plt.axes(projection='3d', aspect='equal',
-        xlim = (-10,10), ylim = (-5,15), zlim = (0,20),
+        xlim = (-10,10), ylim = (-15,5), zlim = (0,20),
         xlabel = 'x', ylabel = 'y', zlabel = 'z')
 plotting.axis_equal(ax)
 for p in history["gt_p"]:
